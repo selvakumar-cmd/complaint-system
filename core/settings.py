08d0153ef568d9de77c667d364cdeb3f7e7c3a2f
Selva kumar <selva@example.com>
2026-05-31 13:05:43 +0530

    Support PostgreSQL via DATABASE_URL

--- core/settings.py
+++ core/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL', default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 }
 
 
